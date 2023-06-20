fb2b5bbce4b30413a449b8c5ab4f2daf39d28fc1
Buhm <buhm.han@gmail.com>
2023-06-20 21:04:53 +0900

    decorator를 활용한 authentication 간소화.

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,12 +4,19 @@ from django.shortcuts import render
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 from accountapp.models import HelloWorld
 
+
+has_ownership = [login_required, account_ownership_required]
+
 # Create your views here.
 
+@login_required
 def hello_world(request):
     if request.method == "POST":
         temp = request.POST.get('hello_world_input')
@@ -40,6 +47,8 @@ class AccountDetailView(DetailView):
     template_name = 'accountapp/detail.html'
 
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
@@ -48,6 +57,8 @@ class AccountUpdateView(UpdateView):
     template_name = 'accountapp/update.html'
 
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
