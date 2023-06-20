dc0248b0fda37a3d6d2527c5096efc3c5af450a4
Buhm <buhm.han@gmail.com>
2023-06-20 21:11:43 +0900

    superuser

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -132,6 +132,9 @@ STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
