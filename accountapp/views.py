53bf27d0207616712dd5c9b4ed1f782a76eb1071
Buhm <buhm.han@gmail.com>
2023-06-20 17:51:25 +0900

    user creation form

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 from accountapp.models import HelloWorld
 
@@ -21,3 +24,10 @@ def hello_world(request):
         return render(request, 'accountapp/hello_world.html', context={
             'hello_world_list': hello_world_list
         })
+
+## 미쳤다. user creation. 단순성. 와.........
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
