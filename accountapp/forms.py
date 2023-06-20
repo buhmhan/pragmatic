ebc5bd867228d33c3e66db45a0bc804581e2f731
Buhm <buhm.han@gmail.com>
2023-06-20 20:34:40 +0900

    account delete enabled.

--- accountapp/forms.py
+++ accountapp/forms.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.forms import UserCreationForm
 
 
+# django의 무시무시한 form class 상속!!! 미리 다 만들어져 있다!!
 class AccountUpdateForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['username'].disabled = True
+
+
