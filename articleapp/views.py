4ceb2cfda3a48950c2bc741b302a535614cd84d7
Buhm <buhm.han@gmail.com>
2023-06-21 12:46:44 +0900

    article end.

--- articleapp/views.py
+++ articleapp/views.py
@@ -3,10 +3,11 @@ from django.contrib.auth.decorators import login_required, login_required
 from django.utils.decorators import method_decorator
 from django.urls import reverse, reverse_lazy
 
-from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 from articleapp.decorators import article_ownership_required
 from articleapp.models import Article
 from articleapp.forms import ArticleCreationForm
+
 
 # Create your views here.
 @method_decorator(login_required, 'get')
@@ -49,3 +50,9 @@ class ArticleDeleteView(DeleteView):
     context_object_name = 'target_article'
     template_name = 'articleapp/delete.html'
     
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'articleapp/list.html'
+    paginate_by = 5
+    ordering = ['-created_at']
